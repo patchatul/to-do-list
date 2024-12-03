f142175f68c63ce9065347c8357e8fbd5756021c
Pat Tul <patchatulyakul@gmail.com>
2024-12-03 17:01:48 -0500

    2nd commit add js function+saveData

--- to-do.js
+++ to-do.js
@@ -1,13 +1,36 @@
-const addInput = document.getElementById('input-to-do');
-const addButton = document.getElementById('add');
+const inputBox = document.getElementById('input-box');
 const showList = document.getElementById('list');
 
-addButton.addEventListener("click", () =>{
-    const input = addInput.value;
-    if (input) {
-        const toDoList = document.createElement('li');
-        toDoList.textContent = input;
-        showList.appendChild(toDoList);
-        addInput.value = '';
+function addToList() {
+    if(inputBox.value === ''){
+        alert("Do you wanna add something to the list?");
     }
-});
+    else{
+        let li = document.createElement("li");
+        li.innerHTML =inputBox.value;
+        showList.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "*"; //delete button
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
+}
+
+showList.addEventListener('click', function(e){
+    if (e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tageName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false)
+
+function saveData(){
+    localStorage.setItem('data', list.innerHTML)
+}
+
+function showData(){
+    list.innerHTML = localStorage.getItem('data');
+}
+showData();
